defeaf86a1439324220377223aaff33af8e0f145
Ilya Pogodaev <ispogodaev@edu.hse.ru>
2019-02-03 18:12:50 +0300

    added half_float

--- kr02/kr02-5.c
+++ kr02/kr02-5.c
@@ -1,76 +1,76 @@
-enum { BLOCK_SIZE = 16 };
+#include <stdint.h>
 
-int get_left_1(unsigned short v) {
+int MAX_NOT_INF() {
+    return 0x10000U - 0x10U - 1U;
+}
+
+int MIN_NOT_INF() {
+    return -MAX_NOT_INF();
+}
+
+int get_left_1(uint32_t v) {
     int counter = 0;
-    while (v ^ 0) {
+    while (v) {
         v >>= 1;
         ++counter;
     }
     return counter - 1;
 }
 
-int get_right_1(unsigned short v) {
-    int counter = 0;
-    while (!(v & 1)) {
-        v >>= 1;
-        ++counter;
-    }
-    return counter;
+unsigned short GET_POSITIVE_INF() {
+    return 0x7c00;
 }
 
-enum { HF_SIGNIFICAND_BYTE_SIZE = 10U, BIAS = 15, MANTISSA_SIZE = 10, EXP_SIZE = 5 };
+enum { BIAS = 15U, MANTISSA_SIZE = 10, EXP_SIZE = 5 };
 
-unsigned short round_value(unsigned short value) {
-    uint32_t fractional = value & ((1U << (get_left_1(value) - HF_SIGNIFICAND_BYTE_SIZE)) - 1);
-    while () {
-    }
+enum { MINUS_OR_MASK = 0x8000, NO_MANTISSA_MSB_XOR_MASK = 0x400 };
+
+uint32_t round_to_place(uint32_t v, int place) {
+    v += 1U << (place - 1);
+    v >>= place;
+    return v;
 }
 
 void to_half_float(int value, unsigned short *hf) {
-    if (...) {
-        *hf = POS_INF();
-        return;
-    }
-    if (...) {
-        *hf = NEG_INF();
-        return;
-    }
-    unsigned short hf_value = 0;
-    if (value < 0) {
-        hf_value |= 1U << (BLOCK_SIZE - 1);
-        value *= -1;
-        // It's normalized anyway.
-        uint32_t right1 = get_right_1(value);
-        if (!is_uint_fit(value)) {
-            value = round_value(value, right1);
+    if (value > MAX_NOT_INF() || value < MIN_NOT_INF()) {
+        *hf = GET_POSITIVE_INF();
+        if (value < 0) {
+            *hf |= MINUS_OR_MASK;
         }
-        uint32_t right1 = get_right_1(value);
-        uint32_t left1 = get_left_1(value);
-        uint32_t exp = right1 + BIAS;
-        value ^= 1U << left1;
-        uint32_t mantissa = value >> right1;
-        *hf_value |= (exp << MANTISSA_SIZE);
-        *hf_value |= mantissa;
+        return;
     }
-    *hf = hf_value;
+    if (value == 0) {
+        *hf = 0;
+        return;
+    }
+    *hf ^= *hf;
+    if (value < 0) {
+        value *= -1;
+        *hf |= MINUS_OR_MASK;
+    }
+    uint32_t abs_value = value;
+    uint32_t left_1 = get_left_1(abs_value);
+    uint32_t mantissa = abs_value;
+    int exp = left_1;
+    if (left_1 <= MANTISSA_SIZE) {
+        mantissa <<= MANTISSA_SIZE - left_1;
+    } else {
+        mantissa = round_to_place(mantissa, left_1 - MANTISSA_SIZE);
+    }
+    mantissa ^= NO_MANTISSA_MSB_XOR_MASK;
+    *hf |= mantissa;
+    *hf |= (exp + BIAS) << MANTISSA_SIZE;
     return;
 }
 
-int is_uint_fit(unsigned short v) {
-    if (v == 0U) {
-        return 1;
-    }
-    return get_left_1(v) - get_right_1(v) <= HF_SIGNIFICAND_BYTE_SIZE;
-}
+#include <stdio.h>
 
 int main() {
     int n;
-    unsigned short *hf = malloc(sizeof(unsigned short));
-    while (scanf() == 1) {
-        to_half_float(n, hf);
-        printf("%x\n", *hf);
+    unsigned short result;
+    while (scanf("%d", &n) == 1) {
+        to_half_float(n, &result);
+        printf("%x\n", result);
     }
-    free(hf);
-    return 0;
 }
 
