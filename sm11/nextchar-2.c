a4fcfe253eb27adcbd5dd341789cc2ce4db7a531
eeeeeeeelias <eliasnwr@gmail.com>
2019-02-08 16:19:19 +0300

    finished nextchar-2

--- sm11/nextchar-2.c
+++ sm11/nextchar-2.c
@@ -1,6 +1,8 @@
 #include <asm/unistd_32.h>
 
-static unsigned char buffer[10000];
+enum { BUFFER_SIZE = 4 * 1024 };
+
+static unsigned char buffer[BUFFER_SIZE];
 
 struct FileReadState {
     int fd;
@@ -11,45 +13,31 @@ struct FileReadState {
     int written;
 };
 
-enum { BUFFER_SIZE = 4 * 1024 };
-
-struct FileReadState state = {
+static struct FileReadState state = {
     0, buffer, BUFFER_SIZE, -2, 0, 0
 };
 
 struct FileReadState *stin = &state;
 
-void write_debug(char ch) {
-    int code;
-    asm volatile(
-        "mov %1, %%eax\n"
-        "mov %2, %%ebx\n"
-        "mov %3, %%ecx\n"
-        "mov %4, %%edx\n"
-        "int $0x80\n"
-        "mov %%eax, %0\n"
-        : "=g" (code)
-        : "g" (__NR_write), "g" (1), "g" (&ch), "g" (1)
-        : "%eax", "%ebx", "%ecx", "%edx", "memory"
-    );
-}
-
 int nextchar() {
     if (stin->lc == -1) {
-        write_debug('e');
         // No more read for ever!
         return -1;
     }
     int code = 0;
     if (stin->written == stin->filled) {
-        write_debug('=');
         if (stin->filled == stin->bufsize) {
             // Clean the buffer.
             stin->filled = stin->written = 0;
         }
         // Read bufsize - filled.
         asm volatile(
-            "\n"
+            "mov %1, %%eax\n"
+            "mov %2, %%ebx\n"
+            "mov %3, %%ecx\n"
+            "mov %4, %%edx\n"
+            "int $0x80\n"
+            "mov %%eax, %0\n"
             : "=g"(code)
             : "g" (__NR_read), "g" (0), "g" (stin->buf + stin->filled), "g" (stin->bufsize - stin->filled)
             : "%eax", "%ebx", "%ecx", "%edx", "memory"
@@ -64,52 +52,10 @@ int nextchar() {
     }
     stin->lc = stin->buf[stin->written];
     ++(stin->written);
-    write_debug('h');
     return stin->lc;
 }
 
 int lastchar() {
     return stin->lc;
-}
-
-void _start() {
-    int ch;
-    int code;
-    for (int i = 0; i != 2; ++i) {
-    // while ((ch = nextchar()) != -1) {
-    
-        ch = nextchar();
-        asm volatile(
-            "mov %1, %%eax\n"
-            "mov %2, %%ebx\n"
-            "mov %3, %%ecx\n"
-            "mov %4, %%edx\n"
-            "int $0x80\n"
-            "mov %%eax, %0\n"
-            : "=g" (code)
-            : "g" (__NR_write), "g" (1), "g" (&ch), "g" (1)
-            : "%eax", "%ebx", "%ecx", "%edx", "memory"
-        );
-        /*
-        asm volatile(
-            "mov %1, %%eax\n"
-            "mov %2, %%ebx\n"
-            "mov %3, %%ecx\n"
-            "mov %4, %%edx\n"
-            "int $0x80\n"
-            "mov %%eax, %0\n"
-            : "=g" (code)
-            : "g" (__NR_write), "g" (2), "g" (&ch), "g" (1)
-            : "%eax", "%ebx", "%ecx", "%edx", "memory"
-        );
-        */
-    }
-    asm volatile(
-        "mov %0, %%eax\n"
-        "xor %%ebx, %%ebx\n"
-        :
-        : "g" (__NR_exit)
-        : "%eax", "%ebx", "memory"
-    );
 }
 
