9ea4d92bf8cfc52b02130b7f94e49146e2726005
eeeeeeeelias <eliasnwr@gmail.com>
2019-02-13 18:01:14 +0300

    finished writechar-2

--- sm11/writechar-2.c
+++ sm11/writechar-2.c
@@ -1,5 +1,9 @@
 #include <asm/unistd_32.h>
 
+enum { BUFFER_SIZE = 4 * 1024 };
+
+static unsigned char buffer[BUFFER_SIZE];
+
 struct FileWriteState
 {
     int fd;
@@ -12,56 +16,16 @@ struct FileWriteState state = {
     1, buffer, BUFFER_SIZE, 0
 };
 
-struct FileWriteState *stout;
+struct FileWriteState *stout = &state;
 
-enum { BUFFER_SIZE = 4 * 1024 };
-
-// Когда вызывается:
-// Пишем один символ в буфер.
-// Далее: либо буфер полностью заполнен (filled == bufsize), тогда:
-// вызываем flush
-// Либо буфер заполнен не полностью (filled < bufsize), тогда:
-// ничего не делаем
-
-void __attribute__((fastcall)) writechar(int c, struct FileWriteState *out) {
-    // %ecx <- c
-    // %edx <- *out
-    out->buffer[out->filled] = c;
-    ++out->filled;
-    if (out->filled == out->bufsize) {
-        flush(out);
-    } 
-}
-
-// делаем write на заполненную часть буфера (смотрим на filled)
-// ставим filled = 0
-
-// Если не так всё просто...
-
-void __attribute__((fastcall)) flush(struct FileWriteState *out) {
-    // %ecx <- *out
-    // 0(%ecx) <- out.fd
-    // 4(%ecx) <- out.buf
+void __attribute__((fastcall)) flush() {
+    struct FileWriteState *out;
+    int code;
     asm volatile(
-        "mov %1, %%eax\n"
-        "mov %2, %%ebx\n"
-        "leal 4(%%ecx), %%ecx"
-        "mov %3, %%edx\n"
-        "int $0x80\n"
-        "mov %%eax, %0\n"
-        : "=g" (code)
-        : "g" (__NR_write), "g" (1), "g" (out->filled)
-        : "%eax", "%ebx", "%ecx", "%edx", "memory"
+        "mov %%ecx, %0\n"
+        : "=g" (out)
+        :: "%ecx", "memory"
     );
-    out->filled = 0;
-}
-
-// А возможно так просто?
-
-void __attribute__((fastcall)) flush(struct FileWriteState *out) {
-    // %ecx <- *out
-    // 0(%ecx) <- out.fd
-    // 4(%ecx) <- out.buf
     asm volatile(
         "mov %1, %%eax\n"
         "mov %2, %%ebx\n"
@@ -76,7 +40,32 @@ void __attribute__((fastcall)) flush(struct FileWriteState *out) {
     out->filled = 0;
 }
 
-void flush(struct FileWriteState *out) {
-
+void __attribute__((fastcall)) writechar() {
+    int c;
+    struct FileWriteState *out;
+    asm volatile(
+        "movl %%ecx, %0\n"
+        "movl %%edx, %1\n"
+        : "=g" (c), "=g" (out)
+        :: "memory"
+    );
+    if (out->filled == out->bufsize) {
+        asm volatile(
+            "mov %0, %%ecx"
+            :: "g" (out)
+            : "%ecx", "memory"
+        );
+        flush();
+    }
+    out->buf[out->filled] = c;
+    ++out->filled;
+    if (out->filled == out->bufsize) {
+        asm volatile(
+            "mov %0, %%ecx"
+            :: "g" (out)
+            : "%ecx", "memory"
+        );
+        flush();
+    } 
 }
 
