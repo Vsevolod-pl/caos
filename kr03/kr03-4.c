502e9aa8235ed75d34f7583771acb50442ebba17
eeeeeeeelias <eliasnwr@gmail.com>
2019-02-14 11:58:54 +0300

    finished kr03-4 (dup_elem)

--- kr03/kr03-4.c
+++ kr03/kr03-4.c
@@ -1,11 +1,61 @@
 #include <errno.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Elem
+{
+    struct Elem *next;
+    char *str;
+};
+
+int is_int32_t(struct Elem *item, int32_t *value) {
+    char *eptr;
+    errno = 0;
+    int64_t test_value = strtol(item->str, &eptr, 10);
+    if (!*(item->str) || *eptr || errno) {
+        return 0;
+    } else if (test_value == (int32_t)test_value) {
+        *value = test_value;
+        return 1;
+    } else {
+        // Too big for int32_t.
+        return 0;
+    }
+}
+
+enum { MAX_INT32_LENGTH = 11 };
+
+int32_t MAX_INT32() {
+    return ~0U >> 1;
+}
+
+int32_t MIN_INT32() {
+    return ~MAX_INT32();
+}
 
 struct Elem *dup_elem(struct Elem *head) {
-    char *eptr;
-    errno = 0; // очищаем errno, так как при успехе errno не модифицируется
-    long value = strtol(str, &eptr, 10);
-    if (!*str || *eptr || errno) {
-        // ошибка преобразования из строки в число
+    // Check for empty list.
+    if (!head) {
+        return head;
     }
+    struct Elem *prev = NULL, *current = head;
+    int32_t value;
+    while (current) {
+        if (is_int32_t(current, &value) && value != MIN_INT32()) {
+            struct Elem *new_item = malloc(sizeof(struct Elem));
+            if (current == head) {
+                head = new_item;
+            } else {
+                prev->next = new_item;
+            }
+            new_item->next = current;
+            new_item->str = malloc(sizeof(char) * (MAX_INT32_LENGTH + 1));
+            sprintf(new_item->str, "%d", value - 1);
+        }
+        prev = current;
+        current = current->next;
+    }
+    return head;
 }
 
