cea37ebac5237759535f732b5a121a9954b60393
eeeeeeeelias <eliasnwr@gmail.com>
2019-02-15 20:55:24 +0300

    added main for read-file-fd-1

---
+++ sm12/read-file-fd-1-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "read-file-fd-1.c"
+
+int main(int argc, char *argv[]) {
+    int fd = open(argv[1], O_RDONLY);
+    struct FileContent file = read_file(fd);
+    if (file.data) {
+        printf("%s\n", file.data);
+        fprintf(stderr, "file.size == %zd\n", file.size);
+    } else {
+        fprintf(stderr, "file.data == NULL\n");
+        fprintf(stderr, "file.size == %zd\n", file.size);
+    }
+    close(fd);
+}
+
