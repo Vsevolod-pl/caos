b1d6f9e92fef294358f9b52ea1a7e9f9f75c542b
eeeeeeeelias <eliasnwr@gmail.com>
2019-02-16 22:53:01 +0300

    added loop instead of my dumbness

--- kr03/kr03-4-dup_elem-main.c
+++ kr03/kr03-4-dup_elem-main.c
@@ -2,7 +2,7 @@
 #include "kr03-4.c"
 
 int main() {
-    char *strings[24] = {
+    char *strings[] = {
         "", "2147483647", "2147483648", "-2147483647", "-2147483648", "-2147483649",
         "+10", "+10 ", " +10",
         "+ 10", "+ 10 ", " + 10",
@@ -11,28 +11,16 @@ int main() {
         "-03", "-03 ", " -03",
         "alpha", "5x",
     };
+
     struct Elem test[19];
-    test[0].next = &test[1];
-    test[1].next = &test[2];
-    test[2].next = &test[3];
-    test[3].next = &test[4];
-    test[4].next = &test[5];
-    test[5].next = &test[6];
-    test[6].next = &test[7];
-    test[7].next = &test[8];
-    test[8].next = &test[9];
-    test[9].next = &test[10];
-    test[10].next = &test[11];
-    test[11].next = &test[12];
-    test[12].next = &test[13];
-    test[13].next = &test[14];
-    test[14].next = &test[15];
-    test[15].next = &test[16];
-    test[16].next = &test[17];
-    test[17].next = &test[18];
-    test[18].next = NULL;
-    for (int i = 0; i != 19; ++i) {
-        test[i].str = strings[i];
+    int i;
+    for (i = 0; i != 18; ++i) {
+        test[i].next = &test[i + 1];
+    }
+    test[i].next = NULL;
+
+    for (int j = 0; j != i + 1; ++j) {
+        test[j].str = strings[j];
     }
     struct Elem *new_head = dup_elem(&test[0]);
 
