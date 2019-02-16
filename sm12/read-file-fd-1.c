b34a8ee4c0cbed4af27b0d6d28944b27a1a30af2
eeeeeeeelias <eliasnwr@gmail.com>
2019-02-16 20:48:53 +0300

    removed structure in read-file-fd-1

--- sm12/read-file-fd-1.c
+++ sm12/read-file-fd-1.c
@@ -4,11 +4,6 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-struct FileContent {
-    ssize_t size;
-    char *data;
-};
-
 enum { BUFFER_SIZE = 4096 };
 
 struct FileContent read_file(int fd) {
