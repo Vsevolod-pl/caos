b0c813c0ce592e9d97f03c83b4ead8c6e629e528
eeeeeeeelias <eliasnwr@gmail.com>
2019-02-20 16:34:56 +0300

    finished reverse-lines, removed debug

--- kr04/kr04-5-reverse-lines.c
+++ kr04/kr04-5-reverse-lines.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <asm/unistd_32.h>
 
 int my_read(int in_fd, unsigned char *out, int size) {
@@ -16,34 +16,33 @@ int my_read(int in_fd, unsigned char *out, int size) {
         : "%eax", "%ebx", "%ecx", "%edx", "memory"
     );
     return errcode;
-    // return read(in_fd, out, size);
+}
+
+int my_write(int out_fd, const unsigned char *out, int size) {
+    int errcode = 0;
+    asm volatile(
+        "mov %1, %%eax\n"
+        "mov %2, %%ebx\n"
+        "mov %3, %%ecx\n"
+        "mov %4, %%edx\n"
+        "int $0x80\n"
+        "mov %%eax, %0\n"
+        : "=g" (errcode)
+        : "g" (__NR_write), "g" (out_fd), "g" (out), "g" (size)
+        : "%eax", "%ebx", "%ecx", "%edx", "memory"
+    );
+    return errcode;
 }
 
 void my_exit(int exitcode) {
     asm volatile(
-        "mov %0, %%eax\n"
+        "movl %0, %%eax\n"
         "movl %1, %%ebx\n"
         "int $0x80\n"
         :: "g" (__NR_exit), "g" (exitcode)
-        : "%eax", "%ebx"
+        : "%eax", "%ebx", "memory"
     );
 }
-
-
-/*
-void write_data(int out_fd, void *data, int data_size) {
-    unsigned char buf[BUF_SIZE];
-    ssize_t err_code;
-    while ((err_code = my_read()) > 0) {
-    }
-    if (*(data + data_size - 1) != '\n') {
-        char nl = '\n';
-        my_write(out_fd, &nl, 1);
-    }
-}
-*/
-
-enum { BUF_SIZE = 4096 };
 
 void *my_brk(void *newbrk) {
     void *result = NULL;
@@ -59,6 +58,8 @@ void *my_brk(void *newbrk) {
     return result;
 }
 
+enum { BUF_SIZE = 4096 };
+
 int read_data(int in_fd, unsigned char **pdata) {
     unsigned char *data = (unsigned char *)my_brk(NULL);
     ssize_t num_read;
@@ -66,6 +67,7 @@ int read_data(int in_fd, unsigned char **pdata) {
     int current_size = 0;
     do {
         new_data = (unsigned char *)my_brk(data + current_size + BUF_SIZE);
+        if (new_data) {}
         current_size += BUF_SIZE;
         num_read = my_read(in_fd, data + current_size - BUF_SIZE, BUF_SIZE);
         if (num_read > 0) {
@@ -90,24 +92,32 @@ int read_data(int in_fd, unsigned char **pdata) {
     return current_size;
 }
 
-void start() {
-    unsigned char *data = NULL;
-    // 0 == stdin
-    int data_size = read_data(0, &data);
-    fprintf(stderr, "data_size == %d\n", data_size);
-    fprintf(stderr, "%c\n", data[10]);
-    for (int i = 0; i != data_size; ++i) {
-        fprintf(stderr, "%c", data[i]);
+struct Data {
+    unsigned char *data;
+    int size;
+};
+
+void write_reversed_data(int out_fd, struct Data *data) {
+    int last_nl = data->size - 1;
+    for (int i = data->size - 1; i > 0; --i) {
+        if (data->data[i - 1] == '\n') {
+            my_write(out_fd, data->data + i, last_nl - i + 1);
+            last_nl = i - 1;
+        }
     }
-    fprintf(stderr);
+    my_write(out_fd, data->data, last_nl + 1);
+}
+
+void _start() {
+    struct Data data;
+    // 0 == stdin
+    data.size = read_data(0, &data.data);
+    if (data.size > 0) {
+        write_reversed_data(1, &data);
+    }
     // 1 == stdout
-    // write_data(1, data, data_size);
-    data = (unsigned char *)my_brk(data);
-    fprintf(stderr, "data == %p\n", data);
+    data.data = (unsigned char *)my_brk(data.data);
     my_exit(0);
 }
 
-int main() {
-    start();
-    return 0;
-}
+
