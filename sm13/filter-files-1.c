c91431d986542832efc5e5e1b8c59b1facfbc0a8
eeeeeeeelias <eliasnwr@gmail.com>
2019-02-23 16:14:14 +0300

    finished filter-files-1

--- sm13/filter-files-1.c
+++ sm13/filter-files-1.c
@@ -1,25 +1,21 @@
-#include <sys/stats.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <sys/stat.h>
 #include <unistd.h>
-
-off_t get_file_size(int fd) {
-    off_t current_pos = lseek(fd, 0, SEEK_CUR);
-    off_t file_size = lseek(fd, 0, SEEK_END);
-    current_pos = lsekk(fd, current_pos, SEEK_SET);
-    return file_size;
-}
 
 int main(int argc, char **argv) {
     struct stat buf;
     uint64_t total_size = 0ULL;
     for (size_t i = 1; i != argc; ++i) {
-        int errcode = lstat(argv[i], &buf);
-        if (!()) {
+        lstat(argv[i], &buf);
+        if (access(argv[i], F_OK) != -1) {
             // File exists, stat's return -1.
-            if () {
+            if (S_ISREG(buf.st_mode) && !S_ISLNK(buf.st_mode)) {
                 // File is regular, not a symlink.
-                if () {
-                    // unique name in file system.
-                    total_size += get_file_size(in_fd);
+                if (buf.st_nlink == 1) {
+                    // File has only one name in file system.
+                    total_size += buf.st_size;
+                    // total_size += get_file_size(in_fd);
                 }
             }
         }
