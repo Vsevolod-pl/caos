c92c71b3036c73fce31960bd24b04b90a1c4f5d4
eeeeeeeelias <eliasnwr@gmail.com>
2019-02-24 13:49:33 +0300

    finished plus-time-1

--- sm13/plus-time-1.c
+++ sm13/plus-time-1.c
@@ -9,19 +9,21 @@ int main() {
     int32_t days_after;
     while (scanf("%d", &days_after) == 1) {
         int64_t seconds_after = (int64_t)days_after * SECONDS_PER_DAY;
-        seconds_after += current_time;
-        if ((time_t)seconds_after == seconds_after) {
-            time_t c = seconds_after;
-            struct tm *p = localtime(&c);
-            printf(
-                "%04d-%02d-%02d\n",
-                p->tm_year + 1900,
-                p->tm_mon + 1,
-                p->tm_mday
-            );
-        } else {
-            printf("OVERFLOW\n");
+        if ((int32_t)seconds_after == seconds_after && seconds_after >= INT32_MIN && seconds_after <= INT32_MAX) {
+            seconds_after += current_time;
+            if ((time_t)seconds_after == seconds_after && seconds_after >= INT32_MIN && seconds_after <= INT32_MAX) {
+                time_t augmented_time = seconds_after;
+                struct tm *p = localtime(&augmented_time);
+                printf(
+                    "%04d-%02d-%02d\n",
+                    p->tm_year + 1900,
+                    p->tm_mon + 1,
+                    p->tm_mday
+                );
+                continue;
+            }
         }
+        printf("OVERFLOW\n");
     }
 }
 
