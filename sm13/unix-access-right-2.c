1de98594b84d9a259116be511d9839db3f469bc3
eeeeeeeelias <eliasnwr@gmail.com>
2019-02-25 16:53:25 +0300

    started unix-access-right-2

---
+++ sm13/unix-access-right-2.c
@@ -0,0 +1,12 @@
+#include <sys/stat.h>
+
+struct Task
+{
+    unsigned uid;
+    int gid_count;
+    unsigned *gids;
+};
+
+int myaccess(const struct stat *stb, const struct Task *task, int access) {
+    return 42;
+}
