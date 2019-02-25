4b7435efcfe86f85e54f554de026f980060b4d26
eeeeeeeelias <eliasnwr@gmail.com>
2019-02-25 16:21:03 +0300

    removed debug fprintf

--- sm13/time-diff-1.c
+++ sm13/time-diff-1.c
@@ -4,31 +4,6 @@
 #include <time.h>
 #include <unistd.h>
 
-enum { MAX_LINE_LENGTH = 1000 };
-
-size_t my_getline(int in_fd, unsigned char *s) {
-    fprintf(stderr, "Start getline()\n");
-    ssize_t num_bytes = read(in_fd, s, MAX_LINE_LENGTH);
-    fprintf(stderr, "num_bytes == %d\n", num_bytes);
-    if (num_bytes <= 0) {
-        return 0;
-    }
-    size_t i;
-    for (i = 0; i != num_bytes; ++i) {
-        if (s[i] == '\n') {
-            s[i] = '\0';
-            fprintf(stderr, "num_bytes, i == %d, %d\n", num_bytes, i);
-            int pos = lseek(in_fd, i + 1 - num_bytes, SEEK_CUR);
-            fprintf(stderr, "pos == %d\n", pos);
-            fprintf(stderr, "num_bytes == %d\n", num_bytes);
-            return i;
-        }
-    }
-    s[i] = '\0';
-    fprintf(stderr, "num_bytes == %d\n", num_bytes);
-    return num_bytes;
-}
-
 enum { YEAR_SIZE = 4, MONTH_SIZE = 2, DAY_SIZE = 2 };
 enum { HOUR_SIZE = 2, MINUTE_SIZE = 2, SECOND_SIZE = 2 };
 
@@ -61,11 +36,26 @@ struct tm parse_tm(const unsigned char *s, size_t size) {
     i += MINUTE_SIZE + 1;
     time.tm_sec = to_int(s + i, SECOND_SIZE) - SECOND_SHIFT;
     time.tm_isdst = -1;
-    // DEBUG
-    fprintf(stderr, "%02d.%02d.%04d, %02d:%02d:%02d\n",
-            time.tm_mday, time.tm_mon, time.tm_year, time.tm_hour, time.tm_min, time.tm_sec);
-    // DEBUG
     return time;
+}
+
+enum { MAX_LINE_LENGTH = 1000 };
+
+size_t my_getline(int in_fd, unsigned char *s) {
+    ssize_t num_bytes = read(in_fd, s, MAX_LINE_LENGTH + 1);
+    if (num_bytes <= 0) {
+        return 0;
+    }
+    size_t i;
+    for (i = 0; i != num_bytes; ++i) {
+        if (s[i] == '\n') {
+            s[i] = '\0';
+            lseek(in_fd, i + 1 - num_bytes, SEEK_CUR);
+            return i;
+        }
+    }
+    s[i] = '\0';
+    return num_bytes;
 }
 
 int main(int argc, char **argv) {
@@ -78,13 +68,11 @@ int main(int argc, char **argv) {
     struct tm tm_time = parse_tm(buf, line_length);
     time_t prev = mktime(&tm_time);
     while ((line_length = my_getline(in_fd, buf)) != 0) {
-        fprintf(stderr, "Stop getline()\n");
         struct tm tm_time = parse_tm(buf, line_length);
         time_t current = mktime(&tm_time);
         printf("%d\n", (int32_t)current - (int32_t)prev);
         prev = current;
     }
-    fprintf(stderr, "Stop getline()\n");
     close(in_fd);
     return 0;
 }
