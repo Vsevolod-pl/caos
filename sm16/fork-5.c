c63883765ce5c09c7214b05d6c0f1aa8cb71b207
eeeeeeeelias <eliasnwr@gmail.com>
2019-03-19 23:57:58 +0300

    finished fork-5

--- sm16/fork-5.c
+++ sm16/fork-5.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
@@ -7,20 +8,25 @@ int main() {
     if (scanf("%d", &num_childs) != 1) {
         return 1;
     }
-    // printf("1");
-    for (size_t i = 1; i != num_childs; ++i) {
+    for (size_t i = 0; i != num_childs; ++i) {
+        if (i + 1 == num_childs) {
+            printf("%d\n", i + 1);
+        } else {
+            printf("%d ", i + 1);
+        }
+        // fflush(stdout);
         pid_t pid = fork();
         if (pid < 0) {
             return 1;
         }
         if (!pid) {
-            fprintf(stderr, "%d is son of %d\n", getpid(), getppid());
-            // printf(" %d", i);
+            // child
+            continue;
         } else {
             wait(NULL);
+            exit(0);
         }
     }
-    printf("\n");
-    return 0;
+    exit(0);
 }
 
